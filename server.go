be0b93c3e1c7d745d8cf78f01302b2a13e12f7de
gclinoz <t96994024@gs.ncku.edu.tw>
2026-07-27 09:45:52 +0800

    CH1-L3: What is Observability

--- server.go
+++ server.go
@@ -47,6 +47,9 @@ func (s *server) start() error {
 	if err != nil {
 		return err
 	}
+	addrTCP := ln.Addr().(*net.TCPAddr)
+	fmt.Printf("Linko is running on http://localhost:%d\n", addrTCP.Port)
+
 	if err := s.httpServer.Serve(ln); !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
@@ -54,6 +57,7 @@ func (s *server) start() error {
 }
 
 func (s *server) shutdown(ctx context.Context) error {
+	fmt.Println("Linko is shutting down")
 	return s.httpServer.Shutdown(ctx)
 }
 
